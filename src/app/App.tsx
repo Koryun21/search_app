bfb52435d4fd37ea6269b3fa26d381cffd1f03e5
Koryun <koryunsirunyan@gmail.com>
2024-01-27 01:25:29 +0400

    wrote base redux configuration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Provider} from "react-redux";
+import {store} from "./store";
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
       index
-    </div>
+    </Provider>
   );
 }
 
